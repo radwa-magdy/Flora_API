d263a829a51b2c1af16d1420e7236fb01ad7cc21
Radwa Magdy <radwa.ali2048@gmail.com>
2026-05-09 20:47:12 +0300

    Update productsDashboard_api.php

--- api/productsDashboard_api.php
+++ api/productsDashboard_api.php
@@ -11,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
 }
 
 require_once "productsDashboard_functions.php";
-
+require_once '../config/db.php';
 
 // =========================================================
 // PARSE ROUTE
